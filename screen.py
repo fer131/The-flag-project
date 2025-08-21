aaf113db1159dc3d812d7468c151f9c74b539dc7
gadi <gadi212007@gmail.com>
2025-08-21 15:19:35 +0300

    grass

--- screen.py
+++ screen.py
@@ -2,13 +2,25 @@ import pygame
 
 import consts
 
+
 screen = pygame.display.set_mode((1000,500))
+imp = pygame.image.load("grass.png")
+image = pygame.transform.scale(imp, consts.GRASS_SIZE)
+
+def random_grass():
+
+
+    pass
 
 def draw_screen():
 
     screen.fill(consts.GREEN)
+    screen.blit(image, (0, 0))
     pygame.display.flip()
 while True:
     draw_screen()
+
+
+
 
 
