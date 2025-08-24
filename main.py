c77192b6439cdd6835780051ca53471af7688e57
fernando <fernandohersh1@gmail.com>
2025-08-24 16:02:22 +0300

    idk

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ state = {
     "player_legs": [],
     "state": consts.RUNNING_STATE,
     "is_window_open": True,
-    "flag_indexes": []
+    "flag_indexes": [],
+    "was_pressed": False
 }
 
 def main():
@@ -23,8 +24,7 @@ def main():
     while state["is_window_open"]:
 
         handle_user_events()
-        print(state["player_body"])
-        print(state["flag_indexes"])
+
         if game_field.touching_mine(state):
             state["state"] = consts.LOSE_STATE
             state["is_window_open"] = False
@@ -34,6 +34,7 @@ def main():
             state["is_window_open"] = False
 
 
+
         soldier.soldier_pos = state["player_pos"]
         soldier.player_pos_calc(state)
         screen.draw_screen(state)
@@ -41,14 +42,18 @@ def main():
         if state["state"] == consts.SHOW_MINES:
             pygame.time.wait(1000)
             pygame.event.clear()
+            state["was_pressed"] = True
             state["state"] = consts.RUNNING_STATE
+
 
 def handle_user_events():
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             state["is_window_open"] = False
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_RETURN and not state["was_pressed"]:
                 state["state"] = consts.SHOW_MINES
             if event.key == pygame.K_LEFT:
                 soldier.soldier_left(state)
