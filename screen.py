d46e1e0befdfd4d53adf5a37c75908e9b9b50aae
gadi <gadi212007@gmail.com>
2025-08-24 16:18:50 +0300

    update.all and finish

--- screen.py
+++ screen.py
@@ -6,8 +6,7 @@ import soldier
 import game_field
 
 CELL = consts.SPACE_BETWEEN_ITEMS
-WIDTH = consts.WINDOW_WIDTH
-HEIGHT=consts.WINDOW_HEIGHT
+
 
 screen = pygame.display.set_mode((1000,500))
 
@@ -115,49 +114,7 @@ def draw_screen(states):
 
 
 
-# def make_cells(r,c,horizontal,length):
-#     cells=[]
-#     if horizontal:
-#         for i in range(length):
-#             cells.append((r,c+i))
-#         else:
-#             for i in range(length):
-#                 cells.append((r+i, c))
-#     return cells
-#
-# def generate_mines():
-#     mines=[]
-#     occupied=set()
-#     while len(mines)<consts.MINE_COUNT:
-#         horizontally=random.choice([True,False])
-#         L=consts.MINE_LINE
-#         if horizontally:
-#             r=random.randint(0,consts.GRID_ROWS-1)
-#             c=random.randint(0,consts.GRID_COLS-1)
-#         else:
-#             r = random.randint(0, consts.GRID_ROWS - L)
-#             c = random.randint(0, consts.GRID_COLS - 1)
-#
-#             cells=make_cells(r,c,horizontally,L)
-#
-#             overlap=False
-#             for cell in cells:
-#                 if cell in occupied:
-#                     overlap=True
-#                     break
-#                 if overlap:
-#                     continue
-#             for cell in cells:
-#                 occupied.add(cell)
-#                 mines.append(cells)
-#     return mines
-#
-# def draw_mines(surface, mines):
-#     for cells in mines:
-#         for(r,c) in cells:
-#             rect=pygame.rect(c*CELL,r*CELL,CELL,CELL)
-#             pygame.draw.rect(surface,consts.MINE_FILL_COLOR, rect)
-#             pygame.draw.rect(surface, consts.MINE_BORDER_COLOR, rect,1)
+
 
 
 
