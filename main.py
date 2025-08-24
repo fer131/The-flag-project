7c82ae852a237443750abbd35d6f2a83d77fd893
fernando <fernandohersh1@gmail.com>
2025-08-24 10:19:17 +0300

    user events

--- main.py
+++ main.py
@@ -6,4 +6,31 @@ state = {
     "player_body": [],
     "player_legs": [],
     "state": consts.RUNNING_STATE,
+    "is_window_open": True
 }
+
+def main():
+    pygame.init()
+    while True:
+        handle_user_events()
+
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            state["is_window_open"] = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("left")
+            if event.key == pygame.K_RIGHT:
+                print("right")
+            if event.key == pygame.K_UP:
+                print("up")
+            if event.key == pygame.K_DOWN:
+                print("down")
+
+
+main()
